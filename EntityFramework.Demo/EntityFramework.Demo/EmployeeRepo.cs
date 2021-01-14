f02a9517326a91c8e614550c83a926265dc873cc
Dinithi_Samarasekara <76129355+DinithiSamarasekara@users.noreply.github.com>
2021-01-14 09:40:14 +0530

    Add files via upload

---
+++ EntityFramework.Demo/EntityFramework.Demo/EmployeeRepo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.Demo
+{
+    internal class EmployeeRepo : IEmployeeRepo
+    {
+        public readonly EmployeeContext employeeContext;
+        
+        public EmployeeRepo(EmployeeContext employeeContext)
+        {
+            this.employeeContext = employeeContext;
+        }
+        Employee Create(string firstNamr,
+            string lastName,
+            string address,
+            string homePhone,
+            string cellPhone)
+        {
+            var response = employeeContext.Add(new Employee
+            {
+                FirstName=firstNamr,
+                LastName=lastName,
+                Address=address,
+                HomePhoned=homePhone,
+                CellPhone=cellPhone
+            });
+            employeeContext.SaveChanges();
+            return response.Entity;
+        }
+        Employee Update(Employee employee)
+        {
+            var response = employeeContext.Update(employee);
+            employeeContext.SaveChanges();
+            return response.Entity;
+        }
+
+        void Delate(Employee employee)
+        {
+            employeeContext.Remove(employee);
+            employeeContext.SaveChanges();
+        }
+    }
+}
---
+++ EntityFramework.Demo/EntityFramework.Demo/IEmployeeRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFramework.Demo
+{
+    internal class IEmployeeRepo
+    {
+        Employee Create(string firstNamr,
+            string lastName,
+            string address,
+            string homePhone,
+            string cellPhone);
+        Employee Update(Employee employee);
+
+        void Delate(Employee employee);
+    }
+}
